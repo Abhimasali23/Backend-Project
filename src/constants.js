496bef141a0558176adaccd2ef79a6773fbe041e
Abhimasali23 <abhimasali23@gmail.com>
2024-03-11 13:17:51 +0530

    All register controller bugs resolved

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export  const  DB_NAME = "abhi"
+export  const  DB_NAME = "abhi";
--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import dotenv from "dotenv"
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
-
-
 dotenv.config({path: './env'})
 
 
@@ -13,8 +11,7 @@ connectDB()
 .then(() => {
     app.listen(process.env.PORT || 8000, () => {
         
-        console.log(`Server is running at port : 
-        ${process.env.PORT}`);
+        console.log(`Server is running at port : ${process.env.PORT}`);
     })
 })
 .catch((err) => {
